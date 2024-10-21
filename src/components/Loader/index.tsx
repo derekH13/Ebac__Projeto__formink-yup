9e6298f1b640bac4dfa1e9b6b30f077fe16e8533
derekH13 <168042164+derekH13@users.noreply.github.com>
2024-10-21 08:29:40 -0300

    edit: form

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,10 +1,8 @@
-import { PacmanLoader } from 'react-spinners'
-import { colors } from '../../styles'
 import { Container } from './styles'
 
 const Loader = () => (
   <Container>
-    <PacmanLoader color={`${colors.LightSalmon}`} />
+    <h1>Carregando...</h1>
   </Container>
 )
 export default Loader
